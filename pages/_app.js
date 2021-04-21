e4cdbde86cc3e276437fd86acc63d75a1f4bc980
Linton <linton.warren01@gmail.com>
2021-04-21 15:57:25 +0200

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,28 @@
-//importing the layout 
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
+//importing the layout
 import Layout from '../components/Layout';
 //importing global css
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  //rendering components as children to the layout
-  return (
-    <Layout>
-    <Component {...pageProps} />
-    </Layout>
-  ) 
+    const router = useRouter();
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            gtag.pageview(url);
+        };
+        router.events.on('routeChangeComplete', handleRouteChange);
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router.events]);
+    //rendering components as children to the layout
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,19 @@
 //importing head to change document title
-import Head from 'next/head'
-//component 
-import Landing from '../components/Landing'
-//home/index css module 
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+//component
+import Landing from '../components/Landing';
+//home/index css module
+import styles from '../styles/Home.module.css';
 
 //index page
 export default function Home() {
-  return (
-    <div className="">
-      <Head>
-        <title>Linton Dorgan Portfolio</title>
-      </Head>
+    return (
+        <div className="">
+            <Head>
+                <title>Linton Dorgan Portfolio</title>
+            </Head>
 
-      <Landing />
-      
-    </div>
-  )
+            <Landing />
+        </div>
+    );
 }
